ed1ff72762d0fcdb1262b04b8a7f1544cac8adc1
Lam giao su <nghiemkhaclam185.mta@gmail.com>
2023-10-24 21:38:48 +0700

    fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -8,7 +8,7 @@ const connect = async () => {
     }
     try {
         await mongoose.connect(
-          process.env.MONGODB_URI,
+          'mongodb+srv://vercel-admin-user:ByDlO0CLmNYrMZDK@cluster0.9eytviv.mongodb.net/test?retryWrites=true&w=majority',
           connectionParams
         )
         console.log("complete")
