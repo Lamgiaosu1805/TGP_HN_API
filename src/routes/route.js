6872a3f42274838936fd70ef4355d7906d13c74b
Lam giao su <nghiemkhaclam185.mta@gmail.com>
2023-10-30 17:20:39 +0700

    improve

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,6 @@ const testController = require('../controllers/TestController');
 function route(app, url) {
 
     // [GET] api/v1/...
-    app.get(`${url}/test`, testController.index);
     app.get(`${url}/linhmucdoan`, LinhMucController.showAllLinhMuc);
     app.get(`${url}/linhmucdoan/page/:page`, LinhMucController.showMoreLinhMuc);
     app.get(`${url}/giaoxu/page/:page`, GiaoXuController.showMoreGiaoXu);
