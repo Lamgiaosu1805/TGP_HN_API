484feb8dfc81ccd7c8f0d8ec9d85f4eb047a7014
Lam giao su <nghiemkhaclam185.mta@gmail.com>
2023-10-31 13:40:41 +0700

    improve get data GX

--- src/controllers/GiaoXuController.js
+++ src/controllers/GiaoXuController.js
@@ -1,4 +1,5 @@
 const GiaoXu = require('../models/GiaoXu');
+const LinhMuc = require('../models/LinhMuc');
 
 class GiaoXuController {
 
@@ -13,19 +14,33 @@ class GiaoXuController {
             .catch(next);
     }
 
-    showMoreGiaoXu(req, res, next) {
+    async showMoreGiaoXu(req, res, next) {
         const page = parseInt(req.params.page);
-        GiaoXu.find().skip((page-1) * 20).limit(20)
-            .then(giaoXus => res.json({
+        try {
+            const listGiaoXu = await GiaoXu.find().skip((page-1) * 20).limit(20);
+            var listGxNew = []
+            for(var i = 0; i < listGiaoXu.length; i++) {
+                const lm = await LinhMuc.findOne({link: listGiaoXu[i].linkLinhMuc})
+                const obj = listGiaoXu[i].toJSON();
+                if(lm) {
+                    obj["LinhMucQx"] = lm;
+                }
+                else {
+                    obj["LinhMucQx"] = {
+                        name: listGiaoXu[i].linkLinhMuc
+                    }
+                }
+                listGxNew.push(obj)
+            }
+            res.json({
                 code: 200,
-                length: giaoXus.length,
+                length: listGiaoXu.length,
                 page: page,
-                data: giaoXus,
-            }))
-            .catch(err => res.json({
-                code: 500,
-                err: err,
-            }))
+                data: listGxNew,
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
     
 }
